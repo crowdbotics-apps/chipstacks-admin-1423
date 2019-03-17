088031af72c2e25c04870f34b5be226f578ce7de
webmaster <webmasterdragon@outlook.com>
2019-03-17 11:06:24 +0700

    gitignore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,9 +1,9 @@
 // Initialize Firebase
 export default {
-  apiKey: "AIzaSyBs-vZF9b_MbCTxLN4HdKZrsO_16OCXOG0",
-  authDomain: "chipstacks-d1cc0.firebaseapp.com",
-  databaseURL: "https://chipstacks-d1cc0.firebaseio.com",
-  projectId: "chipstacks-d1cc0",
-  storageBucket: "chipstacks-d1cc0.appspot.com",
-  messagingSenderId: "606566673594"
+  apiKey: 'AIzaSyBs-vZF9b_MbCTxLN4HdKZrsO_16OCXOG0',
+  authDomain: 'chipstacks-d1cc0.firebaseapp.com',
+  databaseURL: 'https://chipstacks-d1cc0.firebaseio.com',
+  projectId: 'chipstacks-d1cc0',
+  storageBucket: 'chipstacks-d1cc0.appspot.com',
+  messagingSenderId: '606566673594'
 };
